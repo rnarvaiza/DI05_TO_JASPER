2494be3f9def62fffd420d402f88718f31c21d3c
rnarvaiza <rafnarmun@uma.es>
2021-02-05 12:30:07 +0000

    first commit

---
+++ src/main/java/com/iesportada/di05_tarea_narvaiza_munguia_rafael/Conexion.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.iesportada.di05_tarea_narvaiza_munguia_rafael;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author s0ck3
+ */
+public class Conexion {
+    
+    private final String user = "rnarvaiza";
+    private final String password = "032699aA$";
+    private final String url ="jdbc:mysql://10.242.27.110:3306/REPORTE";
+    private Connection con = null;
+    
+    public Connection getConexion(){
+        
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = (Connection) DriverManager.getConnection(this.url, this.user, this.password);
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return con;
+        
+    }
+    
+}
